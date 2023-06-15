970e53eb255b866a46bfd8361a66851dc59ddcfb
brooklvk <brooklvk@byui.edu>
2023-06-15 13:22:55 -0600

    added discount display to product-list and details

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -44,10 +44,13 @@ function productDetailsTemplate(product){
   return `<h3 id="productName">${product.Brand.Name}</h3>
   <h2 class="divider" id="productNameWithoutBrand">${product.NameWithoutBrand}</h2>
   <img id="productImage" class="divider" src="${product.Images.PrimaryExtraLarge}" alt="${product.Name}" />
-  <p class="product-card__price" id="productFinalPrice">${product.FinalPrice}</p>
-  <p class="product__color" id="productColorName">${product.Colors.ColorName}</p>
+  <p class="suggested-price">$${product.SuggestedRetailPrice.toFixed(2)}</p>
+  <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
+  <p class="percent-off">${(product.FinalPrice / product.SuggestedRetailPrice * 100).toFixed(0)}% Off</p>
   <p class="product__description" id="productDescriptionHtmlSimple">${product.DescriptionHtmlSimple}</p>
   <div class="product-detail__add">
     <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
   </div>`;
 }
+
+/* <p class="product__color" id="productColorName">${product.Colors.ColorName}</p> */
